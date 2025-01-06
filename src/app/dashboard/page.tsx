ad7c46b44af1eef893919649cac821b452222598
Ifeanyichukwu Victor Progress <ifeanyiprogress70@gmail.com>
2025-01-06 23:31:12 +0100

    input made

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Button from "@/components/ui/Button";
+import { FC } from "react";
+
+type pageProps = {};
+
+
+const page: FC<pageProps> = ({}) => {
+    return <Button>Hello</Button>}
+
+
+    export default page
